549e5c7de1ce8253f5904da6a3cd7054c7fe98b2
Thomas Munyiri <munyiri.thomas@eclectics.io>
2024-01-21 02:22:36 +0300

    update pending intent flag

--- app/src/main/java/com/tommunyiri/dvtweatherapp/utils/NotificationHelper.kt
+++ app/src/main/java/com/tommunyiri/dvtweatherapp/utils/NotificationHelper.kt
@@ -33,7 +33,7 @@ class NotificationHelper(private val message: String, private val context: Conte
         }
 
         val pendingIntent =
-            PendingIntent.getActivity(context, 0, intent, 0)
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_dvt_weather_notification)
