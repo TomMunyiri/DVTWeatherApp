eeccc728ab704608075f022645b17deaaf340edd
Thomas Munyiri <munyiri.thomas@eclectics.io>
2024-02-26 17:39:16 +0300

    improve permissions handling

--- app/src/main/java/com/tommunyiri/dvtweatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/tommunyiri/dvtweatherapp/presentation/MainActivity.kt
@@ -8,10 +8,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -139,7 +136,7 @@ fun CheckPermissions() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     val navController = rememberNavController()
     DVTWeatherAppTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
--- app/src/main/java/com/tommunyiri/dvtweatherapp/presentation/screens/MainScreen.kt
+++ app/src/main/java/com/tommunyiri/dvtweatherapp/presentation/screens/MainScreen.kt
@@ -1,13 +1,10 @@
 package com.tommunyiri.dvtweatherapp.presentation.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.tommunyiri.dvtweatherapp.presentation.composables.BottomNavigationBar
 import com.tommunyiri.dvtweatherapp.presentation.navigation.MainNavigationScreens
