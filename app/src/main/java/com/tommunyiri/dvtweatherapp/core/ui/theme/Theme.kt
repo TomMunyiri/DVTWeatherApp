30811fa5fd684d4117f8493f7fb5305da0465eda
Thomas Munyiri <munyiri.thomas@eclectics.io>
2024-02-27 23:53:55 +0300

    status bar text color adaptability depending on selected theme

--- app/src/main/java/com/tommunyiri/dvtweatherapp/core/ui/theme/Theme.kt
+++ app/src/main/java/com/tommunyiri/dvtweatherapp/core/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.tommunyiri.dvtweatherapp.core.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -41,7 +43,7 @@ fun DVTWeatherAppTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    var colorScheme = when {
+    val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -53,9 +55,9 @@ fun DVTWeatherAppTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            //val window = (view.context as Activity).window
+            val window = (view.context as Activity).window
             //window.statusBarColor = colorScheme.primary.toArgb()
-            //WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
