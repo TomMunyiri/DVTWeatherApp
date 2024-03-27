d748f9987a9cc2da638b73efae36c4503a3d6256
Thomas Munyiri <munyiri.thomas@eclectics.io>
2024-03-27 19:16:51 +0300

    remove unused functions

--- app/src/main/java/com/tommunyiri/dvtweatherapp/presentation/utils/WeatherUtils.kt
+++ app/src/main/java/com/tommunyiri/dvtweatherapp/presentation/utils/WeatherUtils.kt
@@ -1,7 +1,6 @@
 package com.tommunyiri.dvtweatherapp.presentation.utils
 
 import android.content.Context
-import android.widget.ImageView
 import androidx.compose.ui.graphics.Color
 import com.tommunyiri.dvtweatherapp.R
 import com.tommunyiri.dvtweatherapp.core.ui.theme.cloudy
@@ -18,56 +17,10 @@ import com.tommunyiri.dvtweatherapp.domain.model.Weather
 class WeatherUtils {
     companion object {
         /**
-         * This function helps to dynamically set the [ImageView] depending on the weather
+         * This function helps to dynamically get the [Int] drawable depending on the weather
          * condition [condition] received.
-         * @param iconView the [ImageView] whose icon is to be set
          * @param condition the weather condition
          */
-        fun getIconResources(
-            iconView: ImageView,
-            condition: String?,
-        ) {
-            if (condition != null) {
-                when {
-                    condition.contains("rain", ignoreCase = true) ||
-                        condition.contains("Storm", ignoreCase = true) ||
-                        condition.contains("storm", ignoreCase = true) ||
-                        condition.contains("Rain", ignoreCase = true) ||
-                        condition.contains("ice", ignoreCase = true) ||
-                        condition.contains("Ice", ignoreCase = true) ||
-                        condition.contains("Thunder", ignoreCase = true) ||
-                        condition.contains("thunderstorm", ignoreCase = true) ||
-                        condition.contains("thunderstorm", ignoreCase = true) ||
-                        condition.contains("drizzle", ignoreCase = true) ||
-                        condition.contains("Mist", ignoreCase = true) ||
-                        condition.contains("sleet", ignoreCase = true) ||
-                        condition.contains("snow", ignoreCase = true) ||
-                        condition.contains("Blizzard", ignoreCase = true) ||
-                        condition.contains("thunder", ignoreCase = true) -> {
-                        iconView.setImageResource(R.drawable.rain_2x)
-                    }
-
-                    condition.contains("sun", ignoreCase = true) ||
-                        condition.contains("wind", ignoreCase = true) ||
-                        condition.contains("Wind", ignoreCase = true) ||
-                        condition.contains("Sunny", ignoreCase = true) -> {
-                        iconView.setImageResource(R.drawable.clear_2x)
-                    }
-
-                    condition.contains("cloud", ignoreCase = true) ||
-                        condition.contains("Fog", ignoreCase = true) ||
-                        condition.contains("Cloudy", ignoreCase = true) ||
-                        condition.contains("Clear", ignoreCase = true) ||
-                        condition.contains("Overcast", ignoreCase = true) -> {
-                        iconView.setImageResource(R.drawable.partlysunny_2x)
-                    }
-
-                    else -> {
-                        iconView.setImageResource(R.drawable.clear_2x)
-                    }
-                }
-            }
-        }
 
         fun getWeatherIcon(condition: String): Int {
             return when {
